e6db5c96a31c9bf6a72292bfd47ed2bc2b9ee60e
TMats <takomats@hotmail.com>
2021-06-18 20:42:08 -0400

    resolved the rendering issue.

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import About from "./pages/About";
+import Test from "./components/Test"
+
+function App() {
+    return (
+        <Test />
+    )
+}
+
+export default App;
---
+++ src/components/Test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Test() {
+    return (
+        <div>
+            <h1>Hello There!!</h1>
+            <p>Is this working?</p>
+        </div>
+    );
+}
+
+export default Test;
